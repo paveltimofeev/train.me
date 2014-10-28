4a1b84c2103de123c201828e7aa0c5de51b15e5c
paveltimofeev <pavel.timofeev@hotmail.com>
2014-10-29 00:55:29 +0400

    Added basic features

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -10,3 +10,49 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
+angular.module('trainme', []);
+angular.module('trainme').controller('exiciseViewCtrl', ['$scope', function exiciseViewCtrl($scope){
+	
+	$scope.exiciseName 		= 'Squat';
+	$scope.exiciseTarget 	= '"To do 200 squats"';
+	$scope.todayTask 		= 'Lets Do 30 squats';
+	$scope.todayMotivation 	= 'I think it is enought for start';
+	$scope.todayProgresstodayProgress = 'day 1, left 29';
+	
+	$scope.Done = function Done(){
+		
+	};
+	
+	$scope.Fault = function Fault(){
+		
+	};
+	
+	$scope.RedirectToHistory = function RedirectToHistory(){
+		
+	};
+	
+	$scope.ShareExiciseWithFriend = function ShareExiciseWithFriend(email){
+		
+	};
+}]);
+														 
+angular.module('trainme').controller('menuCtrl', ['$scope', function menuCtrl($scope){
+
+	$scope.menu = 
+		[
+			{ 'name':'200 squats', 'link': '/exercise001' },
+			{ 'name':'Abdominal exercises', 'link': '/exercise002'},
+			{ 'name':'Workout...', 'link': null, 'children':
+				[
+					{ 'name':'Bench press', 'link': '/exercise003' },
+			 		{ 'name':'Deadlift', 	'link': '/exercise004' },
+					{ 'name':'Squat', 		'link': '/exercise005' }
+				]
+			},
+			{ 'name':'200 squats', 'link': '/exercise001'},
+		
+		];
+	
+}]);

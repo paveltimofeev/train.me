e9f0d1126f676eb86996b8e1123dce1ea4369153
paveltimofeev <pavel.timofeev@hotmail.com>
2014-10-29 10:14:54 +0400

    added node_modules. fixed angular

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -17,16 +17,19 @@ angular.module('trainme').controller('exiciseViewCtrl', ['$scope', function exic
 	
 	$scope.exiciseName 		= 'Squat';
 	$scope.exiciseTarget 	= '"To do 200 squats"';
-	$scope.todayTask 		= 'Lets Do 30 squats';
-	$scope.todayMotivation 	= 'I think it is enought for start';
-	$scope.todayProgresstodayProgress = 'day 1, left 29';
+	$scope.todayTask 		= 'Lets start with 75 squats';
+	$scope.todayMotivation 	= ['I think it is enought for start. Do it now!','Yes, I know, it sounds terrible, but I believe you can! Do it now!'][Math.floor(Math.random()*2)];
+	$scope.todayProgress = 'day 1, left 29';
 	
 	$scope.Done = function Done(){
 		
+		$scope.todayTask = 'Congratilations!';
+		$scope.todayMotivation = ['Now you should rest. Came back tomorrow and I will say you what to do', 'Winners train, losers complain! See you tomorrow'][Math.floor(Math.random()*2)];
 	};
 	
 	$scope.Fault = function Fault(){
-		
+		$scope.todayTask = 'Take a rest and try tomorrow';
+		$scope.todayMotivation 	= 'it is not so hard, I believe you can';
 	};
 	
 	$scope.RedirectToHistory = function RedirectToHistory(){
@@ -43,7 +46,7 @@ angular.module('trainme').controller('menuCtrl', ['$scope', function menuCtrl($s
 	$scope.menu = 
 		[
 			{ 'name':'200 squats', 'link': '/exercise001' },
-			{ 'name':'Abdominal exercises', 'link': '/exercise002'},
+			{ 'name':'Abdominal exercises [REST 20h]', 'link': '/exercise002'},
 			{ 'name':'Workout...', 'link': null, 'children':
 				[
 					{ 'name':'Bench press', 'link': '/exercise003' },
